b46b8c5018bd34820d7d426ef152f52a73d7218f
snsakib <hello@snsakib.com>
2024-03-06 20:04:13 +0600

    fix home btn

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { useRouter, usePathname } from "next/navigation";
+import { useAppContext } from "@/providers/context-provider";
 
 export default function Header() {
+  const { state, dispatch } = useAppContext();
   const router = useRouter();
   const pathname = usePathname();
-  console.log(pathname);
+  console.log(state);
 
   return (
     <div className="menu">
-      {pathname !== "/" && (
-        <button className="menu-btn" onClick={() => router.back()}>
+      {state.mode !== "storefront" && (
+        <button
+          className="menu-btn"
+          onClick={() => dispatch({ type: "storefront" })}
+        >
           &#11013;
         </button>
       )}
-      <button className="menu-btn" onClick={() => router.push("/")}>
+      <button
+        className="menu-btn"
+        onClick={() => dispatch({ type: "storefront" })}
+      >
         Home
       </button>
     </div>
